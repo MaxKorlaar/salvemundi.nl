e7b8efbcf0a79ed8234cfe6f417b8608305f496b
Mick Jasker <m.jasker@student.fontys.nl>
2017-11-20 21:07:40 +0100

    Nav en landing

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,8 @@
-window.
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 750) {
+        $("nav").removeClass("navNoBG");
+    }
+    else {
+        $("nav").addClass("navNoBG")
+    }
+});
