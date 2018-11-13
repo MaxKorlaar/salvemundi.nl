aeefbe245533ee608352c1b0d0b37542887b9804
Niek <niek@keersop.nl>
2018-11-13 21:07:35 +0100

    added some design
    Former-commit-id: 15c50568299096b45afde4ce2f07022eccca3de3

--- resources/lang/nl/merchandise.php
+++ resources/lang/nl/merchandise.php
@@ -3,9 +3,31 @@
 return [
     'title' => 'Merchandise',
     'info' => [
-        'title' => 'Salve Mundi Merchandise',
+        'title' => 'Merchandise',
         'intro' => [
-            'Wil jij ook zo\'n litte shirt of vest?!'
+            'Welkom bij de officiële merchandise pagina van Salve Mundi!\\nMet deze merchandixse kun je shinen op elke gelegenheid en laat je zien dat je onderdeel uit maakt van de studievereniging. Op deze pagina kun je bekijken welke merchandise er beschikbar is en kun je een bestelling plaatsen.'
+        ]
+    ],
+    'collections' => [
+        'title' => "Bekijk hieronder de Salve Mundi merchandise collectie",
+        'items' => [
+            "vest" => [
+                "name" => "Vest",
+                "description" => "Je hebt ze vast en zeker al een keer voorbij zien komen. De eerste editie van het Salve Mundi vest!",
+                "image_url" => "images/merch/vest.jpg",
+                "price" => "De prijs van het vest is €29,-",
+                "bullet_information" => [
+                    "Uniek ontwerp!",
+                    "Lekker zacht en warm",
+                    "Model voor zowel dames als heren!",
+                    "100% katoen???",
+                ],
+                "check_size" => "Bekijk hieronder of jouw maat nog beschikbaar is en bestel het verst!"
+            ],
+            "shirt" => [
+                "name" => "Tshirt"
+            ]
         ]
     ]
+
 ];
