d768ab2a9e54b13e3dcd11ac53c57d11a7b7a5ba
INeedSugar <bastiankpn7800@gmail.com>
2019-06-27 15:17:29 +0200

    Update inschrijfkosten
    Former-commit-id: 49a9fd542f8bdd6a8f9b73fe6bbad0c7c6f672d5

--- config/mollie.php
+++ config/mollie.php
@@ -3,7 +3,7 @@
     return [
         'key'             => env('MOLLIE_KEY', ''),
         'camping_costs'   => "40.00",
-        'signup_costs'    => "25.00",
+        'signup_costs'    => "20.00",
         'renew_costs'     => "20.00",
         'transaction_url' => 'https://www.mollie.com/dashboard/payments/',
         'vat_percentage'      => "21.00"
