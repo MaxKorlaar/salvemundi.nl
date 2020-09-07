88373019d10a7dfaaf7280955a2d8dfbcafd48ce
Max Korlaar <max@maxkorlaar.com>
2020-09-07 17:10:53 +0200

    Update social meta tags

--- resources/lang/nl/meta.php
+++ resources/lang/nl/meta.php
@@ -1,6 +1,6 @@
 <?php
     return [
         'social' => [
-            'description' => 'MediaMeiden is de nieuwe studievereniging van Fontys Hogescholen ICT, opgericht in 2017 door Mohammed Mighiss en Luuk Hendriks. De vereniging organiseert veel activiteiten, zowel studie bemiddelend als voor de lol, of een combinatie van de twee. Naast deze activiteiten verzorgt MediaMeiden ook de introductie van FHICT van top tot teen. Bij MediaMeiden haal je meer uit je studie!'
+            'description' => 'MediaMeiden is geen nieuwe studievereniging van Fontys Hogescholen ICT, opgericht in 2020 door mij. De vereniging organiseert geen activiteiten, zowel studie bemiddelend als voor de lol, of een combinatie van de twee. Naast deze activiteiten verzorgt MediaMeiden ook niet de introductie van FHICT van top tot teen. Bij MediaMeiden haal je meer uit je studie!'
         ],
     ];
