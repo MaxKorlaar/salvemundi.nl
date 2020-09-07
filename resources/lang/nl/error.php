c9d0f1dda74ee41221342abed09d95f0a2be62c6
Max Korlaar <max@maxkorlaar.com>
2020-09-07 17:26:32 +0200

    Update error email

--- resources/lang/nl/error.php
+++ resources/lang/nl/error.php
@@ -7,18 +7,18 @@
         '(Is er een nieuw seizoen van Black Mirror?)',
     ];
     return [
-        '400'                => 'Ongeldige aanvraag',
-        '403'                => 'Toegang geweigerd',
-        '404'                => 'Niet gevonden',
-        'method_not_allowed' => 'Aanvraagmethode niet toegestaan',
-        'no_permission' => 'Je hebt momenteel geen toestemming om deze pagina op te vragen',
+        '400'                  => 'Ongeldige aanvraag',
+        '403'                  => 'Toegang geweigerd',
+        '404'                  => 'Niet gevonden',
+        'method_not_allowed'   => 'Aanvraagmethode niet toegestaan',
+        'no_permission'        => 'Je hebt momenteel geen toestemming om deze pagina op te vragen',
         'something_went_wrong' => 'Er ging iets mis.',
-        'file_not_found' => 'De opgevraagde pagina kon niet worden gevonden',
-        '503' => 'Onderhoud',
-        '500' => 'Serverfout',
-        '501' => 'Niet geïmplementeerd',
-        'not_implemented' => 'Deze functionaliteit is momenteel nog niet beschikbaar',
-        'contact_max' => '<a href="mailto:m.korlaar@fontys.nl">Neem alsjeblieft contact op om dit probleem samen te verhelpen</a>',
-        'website_comment' => 'Vraag of opmerking over de website?',
-        'be_right_back' => "Hoi, we hebben de boel eventjes platgegooid voor onderhoud!\nWe zijn zo weer terug, en altijd te bereiken via media@mediameiden.korlaar.net."
+        'file_not_found'       => 'De opgevraagde pagina kon niet worden gevonden',
+        '503'                  => 'Onderhoud',
+        '500'                  => 'Serverfout',
+        '501'                  => 'Niet geïmplementeerd',
+        'not_implemented'      => 'Deze functionaliteit is momenteel nog niet beschikbaar',
+        'contact_max'          => '<a href="mailto:m.korlaar@student.fontys.nl">Neem alsjeblieft contact op om dit probleem samen te verhelpen</a>',
+        'website_comment'      => 'Vraag of opmerking over de website?',
+        'be_right_back'        => "Hoi, we hebben de boel eventjes platgegooid voor onderhoud!\nWe zijn zo weer terug, en altijd te bereiken via media@mediameiden.korlaar.net."
     ];
